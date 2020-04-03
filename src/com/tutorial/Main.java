cafe8acb9094ff3242b12ec87939960a83837894
hassan.adel <hasanadel555@hotmail.com>
2020-04-03 06:57:46 +0200

    Lambda Expressions

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -7,6 +7,27 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        //lambda expression
+        greet((String message)->{
+            System.out.println("Hello world");
+        });
+
+        greet((message)->{
+            System.out.println("Hello world");
+        });
+
+        //only with single parameter
+        greet(message->{
+            System.out.println("Hello world");
+        });
+
+        //if method body has a single line of code
+        greet(message-> System.out.println("Hello world"));
+
+        //add lambda expression to a parameter
+        Printer printer = message -> System.out.println("Hello world");
+
         greet(new Printer() {
             @Override
             public void print(String message) {
