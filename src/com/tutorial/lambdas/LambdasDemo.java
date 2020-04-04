5f7e3f974efab8c6fca95e2854a4c766d81c9b2c
hassan.adel <hasanadel555@hotmail.com>
2020-04-04 04:35:15 +0200

    Method References

--- src/com/tutorial/lambdas/LambdasDemo.java
+++ src/com/tutorial/lambdas/LambdasDemo.java
@@ -2,9 +2,31 @@ package com.tutorial.lambdas;
 
 public class LambdasDemo {
 
+    private String msg;
+
+    public LambdasDemo(String msg) {
+        this.msg = msg;
+    }
+
+    public void print(String msg){}
+
     public static void show(){
         var prefix = "-";
         greet(message -> System.out.println(prefix + message));
+
+        //method reference: same as the above
+        //Class/Object :: method
+        greet(System.out::println);
+
+        //using objects
+        var demo = new LambdasDemo();
+        greet(message -> demo.print(message));
+        greet(demo::print);
+
+        //using constructor
+        greet(message -> new LambdasDemo(message));
+        greet(LambdasDemo::new);
+
     }
     public static void greet(Printer printer){
         printer.print("Hello world");
